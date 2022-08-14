b70dd481e63d0498dc3a37cad8e56a908b8ef069
tvlas <thiago.gilford@gmail.com>
2022-08-14 16:11:04 -0300

    Home page

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,14 +11,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<List> fetchData() async {
+    var uri =
+        'https://api.nasa.gov/planetary/apod?count=5&api_key=EfDC81je4ESlMwsiVcwq7uDxmO66DJg98o3sbtue';
+    var url = Uri.parse(uri);
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Erro ao carregar dados');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        //backgroundColor: Colors.black,
         centerTitle: true,
-        title: const Text('Nasa Photo of the Day'),
+        title: Text('Nasa Photo of the Day'),
       ),
+      body: FutureBuilder<List>(
+          future: fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            }
+            if (snapshot.hasData) {
+              return RefreshIndicator(
+                onRefresh: () async {
+                  await Future.delayed(Duration(seconds: 1));
+                  setState(() {});
+                },
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (contex, index) {
+                    return ListTile(
+                      title: Container(
+                        height: 150,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Image.network(
+                          snapshot.data![index]['url'],
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      subtitle: Text(
+                        snapshot.data![index]['title'],
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  },
+                ),
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
     );
   }
 }
